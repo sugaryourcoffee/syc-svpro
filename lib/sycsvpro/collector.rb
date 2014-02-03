4dce01a91ed6ab4f2cfff5cf83111dfd84374255
Pierre <pierre@sugaryourcoffee.de>
2014-02-03 21:33:52 +0100

    Collect columns and group them to categories

--- lib/sycsvpro/collector.rb
+++ lib/sycsvpro/collector.rb
@@ -5,29 +5,45 @@ module Sycsvpro
 
   class Collector
     
-    attr_reader :infile, :outfile, :row_filter, :col_filter, :collection
+    attr_reader :infile, :outfile, :row_filter, :collection
 
     def initialize(options={})
       @infile = options[:infile]
       @outfile = options[:outfile]
       @row_filter = RowFilter.new(options[:rows])
-      @col_filter = ColumnFilter.new(options[:cols])
-      @collection = []
+      @collection = {}
+      init_collection(options[:cols])
     end
 
     def execute
       File.new(infile).each_with_index do |line, index|
-        result = col_filter.process(row_filter.process(line, row: index))
-        next if result.nil? or result.chomp.empty?
-        result.chomp.split(';').each do |value|
-          collection << value.chomp if collection.index(value.chomp).nil?
+        row = row_filter.process(line, row: index)
+        next if row.nil? or row.chomp.empty?
+        collection.each do |category, elements|
+          values = elements[:filter].process(row) 
+          values.chomp.split(';').each do |value|
+            elements[:entries] << value.chomp if elements[:entries].index(value.chomp).nil?
+          end
         end
       end
 
       File.open(outfile, 'w') do |out|
-        collection.sort.each { |c| out.puts c }
+        collection.each do |category, elements|
+          out.puts "[#{category}]"
+          elements[:entries].sort.each { |c| out.puts c }
+        end
       end
     end
+
+    private
+
+      def init_collection(column_filter)
+        puts column_filter
+        column_filter.split('+').each do |f|
+          category, filter = f.split(':')
+          collection[category] = { entries: [], filter: ColumnFilter.new(filter) }
+        end 
+      end
   end
 
 end
