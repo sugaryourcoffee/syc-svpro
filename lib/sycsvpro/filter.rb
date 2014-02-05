456b2509bd443fe2cfc6cea496fd350fe23fe68a
Pierre <pierre@sugaryourcoffee.de>
2014-02-05 22:09:29 +0100

    add comparator patterns to filter

--- lib/sycsvpro/filter.rb
+++ lib/sycsvpro/filter.rb
@@ -14,6 +14,9 @@ module Sycsvpro
       return equal($1, args, block)    if id =~ /^(\d+)$/
       return range($1, $2, args, block) if id =~ /^(\d+)-(\d+)$/
       return regex($1, args, block)   if id =~ /^\/(.*)\/$/
+      return col_regex($1, $2, args, block)  if id =~ /^(\d+):\/(.*)\/$/
+      return date($1, $2, $3, args, block)      if id =~ /^(\d+):(<|=|>)(\d+.\d+.\d+)/
+      return date_range($1, $2, $3, args, block) if id =~ /^(\d+):(\d+.\d+.\d+.)-(\d+.\d+.\d+)$/
       super
     end
 
@@ -37,6 +40,15 @@ module Sycsvpro
 
       def regex(value, args, block)
         pattern << value unless pattern.index(value)
+      end
+
+      def col_regex(col, regex, args, block)
+      end
+
+      def date(col, comparator, date, args, block)
+      end
+
+      def date_range(col, start_date, end_date, args, block)
       end
 
   end
