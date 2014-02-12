9c88baa1e3736d4533317c13b3dbfd09ed75e96b
Pierre <pierre@sugaryourcoffee.de>
2014-02-12 08:20:21 +0100

    replace non-UTF-8 chars in ColumnFilter

--- lib/sycsvpro/column_filter.rb
+++ lib/sycsvpro/column_filter.rb
@@ -6,6 +6,7 @@ module Sycsvpro
     
     def process(object, options={})
       return nil if object.nil?
+      object = object.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
       return object if filter.empty? and pivot.empty?
       filtered = object.split(';').values_at(*filter.flatten.uniq)
       pivot_each_column(object.split(';')) do |column, match|
