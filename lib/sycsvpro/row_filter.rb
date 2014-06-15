f81594bb4b17a983a08df9ecea4a6469c33c723d
Pierre <pierre@sugaryourcoffee.de>
2014-06-15 12:08:19 +0200

    add row_filter_spec to primarilly test a boolean filter for rows. Determined the regex for such filter

--- lib/sycsvpro/row_filter.rb
+++ lib/sycsvpro/row_filter.rb
@@ -11,15 +11,23 @@ module Sycsvpro
 
     # Processes the filter on the given row
     def process(object, options={})
+      STDERR.puts "object = #{object}"
       object = unstring(object)
       return object unless has_filter?
       filtered = !filter.flatten.uniq.index(options[:row]).nil?
       pattern.each do |p|
+        STDERR.puts "pattern = #{pattern}"
         filtered = (filtered or !(object =~ Regexp.new(p)).nil?)
       end
       filtered ? object : nil
     end
     
   end
-
+  
+  # Match a logical row filter
+  # Example: 1,2,3,4-5:c1=10&&c2<20||c3>4&&c4=word||c5=2014-3-2&&c6=/\d{3,4}/  
+  # (\d+(?:,\d+|-\d)*):(c\d+[<=>](?:\d{4}-\d{1,2}-\d{1,2}|\d+|\w+|\/.*?\/)(?:(?:&&|\|\||$)c\d+[<=>](?:\d{4}-\d{1,2}-\d{1,2}|\d+|\w+|\/.*?\/))*)
+  # This will match
+  # 1. 1,2,3,4-5
+  # 2. c1=10&&c2<20||c3>4&&c4=word||c5=2014-3-2&&c6=/\d{3,4}/  
 end
---
+++ spec/sycsvpro/row_filter_spec.rb
@@ -0,0 +1,43 @@
+require 'sycsvpro/row_filter'
+
+module Sycsvpro
+
+  describe RowFilter do
+
+    before do
+      @in_file = File.join(File.dirname(__FILE__), "files/in.csv")
+      @out_file = File.join(File.dirname(__FILE__), "files/out.csv")
+    end
+
+    it "should return row string when no filter is set" do
+      row_filter = Sycsvpro::RowFilter.new(nil)
+      row_filter.process("abc", row: 1).should eq "abc"
+    end
+
+    it "should filter rows on index" do
+      rows = "1-5"
+      row_filter = Sycsvpro::RowFilter.new(rows)
+      row_filter.process("abc", row: 1).should eq "abc"
+      row_filter.process("abc", row: 6).should be_nil 
+    end
+
+    it "should filter rows on regex" do
+      rows = "1,\/\\d{2,}\/"
+      row_filter = Sycsvpro::RowFilter.new(rows)
+      row_filter.process("5;50;500", row: 1).should eq "5;50;500"
+      row_filter.process("5;50;500", row: 2).should eq "5;50;500"
+    end
+
+    it "should filter rows on logical expression" do
+      rows = "1:c1>50&&c2=Ruby||c3<10"
+      row_filter = Sycsvpro::RowFilter.new(rows)
+      row_filter.process("a;49;Rub;9").should eq "a;49;Rub;9"
+      row_filter.process("a;51;Ruby;11").should eq "a;51;Ruby;11"
+      row_filter.process("a;49;Ruby;11").should be_nil
+    end
+
+  end
+
+end
+
+ 
