6094c0d0b3d08e7399288be7822298d2e84c2be0
Pierre <pierre@sugaryourcoffee.de>
2014-09-18 20:48:11 +0200

    Add test for matices

---
+++ spec/sycsvpro/vector_spec.rb
@@ -0,0 +1,18 @@
+module Sycsvpro
+
+  describe Vector do
+
+    it "should multiply two matrices"
+
+    it "should add two matrices"
+
+    it "should subtract two matrices"
+
+    it "should devide two matrices"
+
+    it "should process multiple operations on matrices"
+
+  end
+
+end
+
