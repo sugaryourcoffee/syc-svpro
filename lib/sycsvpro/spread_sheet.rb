d6431290bc6cd7a18ecca69dbffdbd83f03b86f3
Pierre <pierre@sugaryourcoffee.de>
2014-10-04 22:45:45 +0200

    extend spread sheet with tests

--- lib/sycsvpro/spread_sheet.rb
+++ lib/sycsvpro/spread_sheet.rb
@@ -1,18 +1,115 @@
 module Sycsvpro
 
+  # A spread sheet is used to do column and row wise calculations between
+  # spread sheets. The calculations can be *, /, + and - where the operations
+  # are conducted between corresponding columns and rows. It is not as with
+  # matrix operations.
+  #
+  # Example:
+  #           [0] [1]          [0] [1]
+  #   A = [0]   1   2  B = [0]   5   6 
+  #       [1]   3   4      [1]   7   8
+  #
+  #               [0]  [1]
+  #   A * B = [0]   5   12  
+  #           [1]  21   32
+  #
+  # If spread sheets are not the same size then the operation is looping through
+  # the smaller spread sheets values
+  #
+  # Example:
+  #           [0] [1]          [0]          [0]
+  #   A = [0]   1   2  B = [0]   5  C = [0]   8
+  #       [1]   3   4      [1]   7
+  #
+  #               [0]  [1]
+  #   A * B = [0]   5   35
+  #           [1]  21   28
+  #
+  #               [0]  [1]
+  #   A * C = [0]   8   16
+  #           [1]  24   32
   class SpreadSheet
 
+    # rows of the spread sheet
     attr_accessor :rows
+    # options of the spread sheet
+    attr_accessor :opts
+    # row labels
+    attr_accessor :row_labels
+    # column labels
+    attr_accessor :col_labels
 
+    # Creates a new spread sheet with rows and optional options.
+    # 
+    #   SpreadSheet.new([A,1,2], [B,3,4], rlabel: true, clabel: false)
+    #
+    # rlabel: first column of the row contains labels if true
+    # clabel: first row are labels if true
+    #
+    # Creates a spread sheet with row labels 'A', 'B' and no column labels
+    #       [0] [1]
+    #   [A]   1   2
+    #   [B]   3   4
+    #
+    #   SpreadSheet.new(['One','Two'],['A',1,2],['B',3,4], 
+    #                   rlabel = true, 
+    #                   clabel = true)
+    #
+    # Creates a spread sheet with row and column labels
+    #
+    #       [One] [Two]
+    #   [A]     1     2
+    #   [B]     3     4
     def initialize(*rows)
+      opts = rows.pop if rows.last.is_a?(::Hash)
+      @opts = opts || {}
       check_validity_of(rows)
+      @row_labels, @col_labels = create_labels(rows)
       @rows = rows
     end
 
     # Returns the dimension [rows, columns] of the spread sheet
     #   SpreadSheet.new([1,2,3], [4,5,6]).dim -> [2,3]
     def dim
-      [rows.size, rows[0].size]
+      [nrows, ncols]
+    end
+
+    # Returns the size of the spread sheet, that is the count of elements
+    def size
+      nrows * ncols
+    end
+
+    # Returns the number of rows
+    def nrows
+      rows.size
+    end
+
+    # Returns the number of columns
+    def ncols
+      rows[0].size
+    end
+
+    # Returns a subset of the spread sheet and returns a new spread sheet with
+    # the result
+    def [](*range)
+      r, c = range
+      r ||= 0..(nrows-1)
+      c ||= 0..(ncols-1)
+ 
+      row_selection = rows.values_at(*r)
+      col_selection = []
+
+      if rows_are_arrays?(row_selection)
+        row_selection.each do |row|
+          values = row.values_at(*c)
+          col_selection << (values.respond_to?(:to_ary) ? values : [values])
+        end 
+      else
+        col_selection << row_selection[*c]
+      end
+
+      SpreadSheet.new(*col_selection)
     end
 
     # Multiplies two spreadsheets column by column and returns a new spread
@@ -65,6 +162,25 @@ module Sycsvpro
       true
     end
     
+    # Prints the spread sheet in a matrix with column labels and row labels. If
+    # no labels are available the column number and row number is printed
+    def to_s
+      col_label_size = ncols.to_s.size
+      row_label_size = nrows.to_s.size
+      col_size = [rows.flatten.collect { |c| c.to_s.size }.max, 
+                  col_label_size + 2                            ].max + 1 
+
+      print(sprintf("%#{row_label_size + 2}s", " "))
+      0.upto(ncols - 1) { |i| print(sprintf("%#{col_size}s", "[#{i}]")) }
+      puts
+
+      rows.each_with_index do |row, i|
+        print(sprintf("[%#{row_label_size}s]", i))
+        row.each { |c| print(sprintf("%#{col_size}s", c)) }
+        puts
+      end
+    end
+    
     private
 
       # Checks whether the rows are valid, that is
@@ -80,8 +196,9 @@ module Sycsvpro
       # Checks whether all rows have the same column size. Returns true if
       # all columns have the same column size
       def same_column_size?(rows)
-        return true if rows.size == 1
-        0.upto(rows.size - 2) do |i| 
+        offset = opts[:c] ? 1 : 0
+        return true if rows.size == 1 + offset
+        (0 + offset).upto(rows.size - 2) do |i| 
           return false unless rows[i].size == rows[i+1].size
         end
         true
@@ -94,13 +211,45 @@ module Sycsvpro
         true
       end
 
+      def create_labels(rows)
+        if opts[:c]
+          col_labels = extract_col_labels(rows)
+        end
+        if opts[:r]
+          row_labels = extract_row_labels(rows)
+        end
+        row_labels = (0..rows.size-1).to_a    unless row_labels
+        col_labels = (0..rows[0].size-1).to_a unless col_labels
+        [row_labels, col_labels]
+      end
+
+      def extract_col_labels(rows)
+        col_labels = rows.shift
+        col_labels.shift if col_labels.size == rows[0].size
+        col_labels
+      end
+
+      def extract_row_labels(rows)
+        row_labels = []
+        rows.each { |row| row_labels << row.shift }
+        row_labels
+      end
+
+      # Conducts the calculation based on the operator
       def process(operator, s)
         result = []
-        row_count, col_count = dim
+        s1_row_count, s1_col_count = dim
+        s2_row_count, s2_col_count = s.dim
+        row_count = [s1_row_count, s2_row_count].max
+        col_count = [s1_col_count, s2_col_count].max
         0.upto(row_count - 1) do |r|
+          r1 = r % s1_row_count
+          r2 = r % s2_row_count
           element = []
           0.upto(col_count - 1) do |c|
-            element << rows[r][c].send(operator, s.rows[r][c])
+            c1 = c % s1_col_count
+            c2 = c % s2_col_count
+            element << rows[r1][c1].send(operator, s.rows[r2][c2])
           end
           result << element
         end
--- spec/sycsvpro/spread_sheet_spec.rb
+++ spec/sycsvpro/spread_sheet_spec.rb
@@ -4,6 +4,7 @@ module Sycsvpro
 
   describe SpreadSheet do
 
+    # Creation of spread sheets
     it "should ensure all rows have the same column size" do
       expect { SpreadSheet.new([1,2], [3,4,5]) }.to raise_error(RuntimeError,
                                              "rows must be of same column size")
@@ -19,6 +20,34 @@ module Sycsvpro
                                                   "needs at least one row")
     end
 
+    # Information about spread sheets
+    
+    it "should return the dimension of a spreadsheet" do
+      s1 = SpreadSheet.new([1,2,3], [3,4,5])
+      s1.dim.should eq [2,3]
+    end
+
+    it "should return the row count" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6], [7,8,9])
+      s1.nrows.should eq 3
+    end
+
+    it "should return the column count" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6], [7,8,9])
+      s1.ncols.should eq 3
+    end
+
+    it "should return the size" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6], [7,8,9])
+      s1.size.should eq 9
+    end
+
+    it "should return default row and column labels" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6], [7,8,9])
+      s1.row_labels.should eq [0,1,2]
+      s1.col_labels.should eq [0,1,2]
+    end
+
     it "should check whether two spread sheets are equal" do
       s1 = SpreadSheet.new([1,2,3], [4,5,6])
       (s1 == s1).should be_true
@@ -26,16 +55,54 @@ module Sycsvpro
       (s1 == s2).should be_false
     end
 
-    it "should return the dimension of a spreadsheet" do
-      s1 = SpreadSheet.new([1,2,3], [3,4,5])
-      s1.dim.should eq [2,3]
+    # Subsetting spread sheets
+    
+    it "should retrieve rows based on row number" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6])
+      s2 = SpreadSheet.new([4,5,6])
+      s1[1,].should eq s2
+      s1[nil,nil].should eq s1
     end
 
-    it "should multiply two spread sheets" do
+    it "should return columns based on column numbers" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6])
+      s2 = SpreadSheet.new([3],[6])
+      s1[nil,2].should eq s2
+    end
+ 
+    it "should return a subset of the table" do
+      s1 = SpreadSheet.new([1,2,3], [4,5,6])
+      s2 = SpreadSheet.new([5,6])
+      s1[1,1..2].should eq s2
+      s1 = SpreadSheet.new([10,11,12], [13,14,15])
+      s2 = SpreadSheet.new([11,12], [14,15])
+      s1[nil,1..2].should eq s2
+      s1 = SpreadSheet.new([16,17,18], [19,20,21])
+      s2 = SpreadSheet.new([16,18], [19,21])
+      s1[nil,[0,2]].should eq s2
+    end
+
+    # Calculating with spread sheets
+
+    it "should multiply two spread sheets of same size" do
       v1 = SpreadSheet.new([1,2],[3,4])
       v2 = SpreadSheet.new([5,6],[7,8])
       v3 = SpreadSheet.new([5,12],[21,32])
       (v1 * v2).should eq v3
+    end
+
+    it "should multiply two spread sheets of different size" do
+      v1 = SpreadSheet.new([2],[4])
+      v2 = SpreadSheet.new([5,6],[7,8])
+      v3 = SpreadSheet.new([10,12],[28,32])
+      (v1 * v2).should eq v3
+      (v2 * v1).should eq v3
+
+      v1 = SpreadSheet.new([2,3,4],[4,5,6])
+      v2 = SpreadSheet.new([5,6],[7,8])
+      v3 = SpreadSheet.new([10,18,20],[28,40,42])
+      (v1 * v2).should eq v3
+      (v2 * v1).should eq v3
     end
 
     it "should add two spread sheets" do
@@ -59,8 +126,26 @@ module Sycsvpro
       (v1 / v2).should eq v3
     end
 
-    it "should multiply a column vector with each column of a spread sheet"
+    # Spread sheets with column and row labels
+  
+    it "should return provided row and column labels" do
+      v1 = SpreadSheet.new(['X','Y'], ['A',1,2],['B',3,4], r: true, c: true)
+      v1.opts[:r].should be_true
+      v1.opts[:c].should be_true
+      v1.row_labels.should eq ['A','B']
+      v1.col_labels.should eq ['X','Y']
+    end
 
+    it "should multiply spread sheets with row labels" do
+    #  v1 = SpreadSheet.new(['A', 1,2],['B', 3,4], rlable: true)
+    #  v2 = SpreadSheet.new([5,6],[7,8])
+    #  v3 = SpreadSheet.new(['A', 5, 12], ['B', 21, 32])
+    #  (v1 * v2).should eq v3
+    end
+
+    it "should multiply spread sheets with column labels"
+
+    it "should multiply spread sheets with row and column labels"
   end
 
 end
