af69c6cd60c751f963dd856e1afa0d489abd235a
Pierre <pierre@sugaryourcoffee.de>
2014-10-12 22:28:02 +0200

    Removed test for character encoding

--- lib/sycsvpro/spread_sheet.rb
+++ lib/sycsvpro/spread_sheet.rb
@@ -421,8 +421,7 @@ module Sycsvpro
         elsif opts[:file]
           File.foreach(opts[:file]) do |line| 
             next if line.chomp.empty?
-            values = line.split(SEMICOLON) rescue str2utf8(line).
-                                                    split(SEMICOLON)
+            values = line.split(SEMICOLON) rescue str2utf8(line).split(SEMICOLON)
             rows << values.collect { |v| 
               v.strip.empty? ? NotAvailable : str2num(v.chomp, opts[:ds])
             }
